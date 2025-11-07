8a2a6792aa42259cdc44e91e325ab7fd12cc40e9
claire <61533589+joooo24@users.noreply.github.com>
2025-11-08 00:32:34 +0900

    create: 필요한 페이지 생성

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,24 @@
 import { Routes, Route } from "react-router-dom";
 import "./App.scss";
+import AppLayout from "./layout/AppLayout";
+import HomePage from "./pages/HomePage/HomePage";
+import MoviePage from "./pages/MoviePage/MoviePage";
+import MovieDetailPage from "./pages/MovieDetailPage/MovieDetailPage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <Routes>
+            <Route path="/" element={<AppLayout />}>
+                <Route index element={<HomePage />} />
+                <Route path="movies">
+                    <Route index element={<MoviePage />} />
+                    <Route path=":id" element={<MovieDetailPage />} />
+                </Route>
+            </Route>
+            <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+    );
 }
 
-export default App
+export default App;
