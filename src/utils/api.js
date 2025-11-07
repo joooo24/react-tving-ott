37aeda3cc42a5b07667dcf147a74b0e95f535da7
claire <61533589+joooo24@users.noreply.github.com>
2025-11-08 01:35:09 +0900

    create: api

---
+++ src/utils/api.js
@@ -0,0 +1,34 @@
+import axios from "axios";
+const API_KEY = process.env.REACT_APP_API_KEY;
+
+const api = axios.create({
+    baseURL: "https://api.themoviedb.org/3",
+    headers: {
+        Accept: 'application/json',
+        Authorization: `Bearer ${API_KEY}`,
+    },
+});
+
+// 요청 인터셉터를 추가
+api.interceptors.request.use(
+    // 요청이 전달되기 전에 작업을 수행
+    function (config) {
+        return config;
+    },
+    // 요청 오류가 있는 경우 작업을 수행
+    function (error) {
+        return Promise.reject(error);
+    }
+);
+
+// 응답 인터셉터를 추가
+api.interceptors.response.use(
+    function (response) {
+        return response;
+    },
+    function (error) {
+        return Promise.reject(error);
+    }
+);
+
+export default api;
