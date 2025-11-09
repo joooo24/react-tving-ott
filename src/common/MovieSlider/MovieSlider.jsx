8eb399a9cfd0061491dee0d94498bffbdd58d265
claire <61533589+joooo24@users.noreply.github.com>
2025-11-09 21:23:13 +0900

    chore: responsive 공통 분리

--- src/common/MovieSlider/MovieSlider.jsx
+++ src/common/MovieSlider/MovieSlider.jsx
@@ -3,25 +3,9 @@ import "./MovieSlider.scss";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 import MovieCard from "../MovieCard/MovieCard";
+import { responsive } from "../../constants/responsive";
 
 const MovieSlider = ({ movie, slideTitle }) => {
-    const responsive = {
-        desktop: {
-            breakpoint: { max: 3000, min: 1024 },
-            items: 3,
-            slidesToSlide: 6,
-        },
-        tablet: {
-            breakpoint: { max: 1024, min: 464 },
-            items: 2,
-            slidesToSlide: 2,
-        },
-        mobile: {
-            breakpoint: { max: 464, min: 0 },
-            items: 1,
-            slidesToSlide: 1,
-        },
-    };
     return (
         <div>
             <h3 className="slide-title">{slideTitle}</h3>
