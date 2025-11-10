d1b46c2bc6a77c66b71ed86e7f45b87081764431
claire <61533589+joooo24@users.noreply.github.com>
2025-11-11 00:41:51 +0900

    feat: 검색 결과 및 페이지네이션

--- src/hooks/useSearchMovie.js
+++ src/hooks/useSearchMovie.js
@@ -1,14 +1,14 @@
 import { useQuery } from "@tanstack/react-query";
 import api from "../utils/api";
 
-const fetchSearchMovies = ({ keyword }) => {
-    return keyword ? api.get(`/search/movie?query=${keyword}`) : api.get(`/movie/popular`);
+const fetchSearchMovies = ({ keyword, page }) => {
+    return keyword ? api.get(`/search/movie?query=${keyword}&page=${page}`) : api.get(`/movie/popular?page=${page}`);
 };
 
-export const useSearchMovieQuery = ({ keyword }) => {
+export const useSearchMovieQuery = ({ keyword, page }) => {
     return useQuery({
-        queryKey: ["movie-search", keyword],
-        queryFn: () => fetchSearchMovies({keyword}),
+        queryKey: ["movie-search", { keyword, page }],
+        queryFn: () => fetchSearchMovies({ keyword, page }),
         select: (results) => results.data,
     });
 };
--- src/pages/MoviePage/MoviePage.jsx
+++ src/pages/MoviePage/MoviePage.jsx
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSearchMovieQuery } from "../../hooks/useSearchMovie";
 import { useSearchParams } from "react-router-dom";
 import { Container, Row, Col } from "react-bootstrap";
 import MovieCard from "../../common/MovieCard/MovieCard";
 import "./MoviePage.scss";
+import ReactPaginate from "react-paginate";
 
 const MoviePage = () => {
     const [query] = useSearchParams();
+    const [page, setPage] = useState(1);
+
     const keyword = query.get("q");
-    console.log("keyword", keyword);
-    const { data, isLoading, isError, error } = useSearchMovieQuery({ keyword });
+
+    const handlePageClick = ({ selected }) => {
+        setPage(selected + 1);
+    };
+
+    const { data, isLoading, isError, error } = useSearchMovieQuery({ keyword, page });
     if (isLoading) {
         return <div>Loading...</div>;
     }
@@ -25,12 +32,42 @@ const MoviePage = () => {
                 </Col>
                 <Col lg={8} xs={12}>
                     <Row className="movie-list">
-                        {data?.results?.map((movie, index) => (
-                            <Col className="movie-item" key={index} lg={4} xs={6}>
-                                <MovieCard movie={movie} />
-                            </Col>
-                        ))}
+                        {data?.results?.length > 0 ? (
+                            <Row className="movie-list">
+                                {data.results.map((movie, index) => (
+                                    <Col className="movie-item" key={index} lg={4} xs={6}>
+                                        <MovieCard movie={movie} />
+                                    </Col>
+                                ))}
+                            </Row>
+                        ) : (
+                            <div style={{ textAlign: "center", padding: "40px", color: "#fff" }}>
+                                검색 결과가 없습니다
+                            </div>
+                        )}
                     </Row>
+
+                    <ReactPaginate
+                        nextLabel=">"
+                        onPageChange={handlePageClick}
+                        pageRangeDisplayed={3}
+                        marginPagesDisplayed={2}
+                        pageCount={data?.total_pages} // 전체페이지
+                        previousLabel="<"
+                        pageClassName="page-item"
+                        pageLinkClassName="page-link"
+                        previousClassName="page-item"
+                        previousLinkClassName="page-link"
+                        nextClassName="page-item"
+                        nextLinkClassName="page-link"
+                        breakLabel="..."
+                        breakClassName="page-item"
+                        breakLinkClassName="page-link"
+                        containerClassName="pagination"
+                        activeClassName="active"
+                        renderOnZeroPageCount={null}
+                        forcePage={page - 1}
+                    />
                 </Col>
             </Row>
         </Container>
